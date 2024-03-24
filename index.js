3e19c6fb6da27e40ba37d74f0358f2d0669870a5
JACKA <jackarcisse@gmail.com>
2024-03-24 19:08:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const main = document.getElementById("main");
+// main.innerHTML = "<h1>Hello, World!</h1>";
+
+
+// element.style.backgroundColor = "red";
+
+// element.textContent = "This is some new text.";
+
+// element.className = "new-class";
+// element.classList.add("another-class");
+
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = "Jacka Cisse is the champion";
